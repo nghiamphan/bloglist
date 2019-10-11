5b01c9931ac94b25f061a9434dc303533fcd7aea
Nghia <nphan@fandm.edu>
2019-10-10 21:54:42 -0700

    populate the the fields username and name for user within the object returned by PUT

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -75,7 +75,9 @@ blogsRouter.put('/:id', async (request, response, next) => {
   }
 
   try {
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    const updatedBlog = await Blog
+      .findByIdAndUpdate(request.params.id, blog, { new: true })
+      .populate('user', { username: 1, name: 1 })
     response.json(updatedBlog)
   } catch (exception) {
     next(exception)
